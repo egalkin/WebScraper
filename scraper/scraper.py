28e5d1f7a907b6b3e145a52c4ce6f7c47c987ce8
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-03 17:40:10 +0300

    Added interaction with statistic collector service

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,13 +1,13 @@
 import hashlib
 import sys
-from datetime import datetime
+
+import requests
 from elasticsearch import Elasticsearch
 
 from es_settings import hh_settings, log_settings
 from put_bodies import create_changes_log
 from scrapping_utils import parse_html, get_pages
-from search_utils import create_index, store_vacancy_record, store_url_record, get_changes, store_change_record, \
-    check_connection
+from search_utils import create_index, store_vacancy_record, store_url_record, get_changes, store_change_record
 
 
 def main():
@@ -27,15 +27,20 @@ def main():
         global_hash = ''
         for r in response:
             global_hash += store_vacancy_record(es, 'hh', r, url)
+        global_hash += 'egor'
         hash_object = hashlib.md5(global_hash.encode())
         changes = get_changes(es, 'log', url)
-        record = create_changes_log(url, hash_object.hexdigest(), len(response))
+        record = create_changes_log(url, hash_object.hexdigest(), len(response) + 25)
         if len(changes) == 0:
             store_change_record(es, 'log', record, url)
+            record['status'] = 'create'
+            requests.post('http://localhost:5000/api/v1/collect', data=record)
         else:
             cur_record_hash = changes[0]['_source']['hash']
             if hash_object.hexdigest() != cur_record_hash:
                 store_change_record(es, 'log', record, url)
+                record['status'] = 'update'
+                requests.post('http://localhost:5000/api/v1/collect', data=record)
 
 
 if __name__ == "__main__":
