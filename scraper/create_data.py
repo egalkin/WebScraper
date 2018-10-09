17f49897098363359ad4d1c45e6b35d595461a1d
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-09 14:52:31 +0300

    Change method name from with get prefix

--- scraper/create_data.py
+++ scraper/create_data.py
@@ -3,7 +3,7 @@ import random
 import string
 
 
-def random_word(length):
+def get_random_word(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
 
@@ -11,4 +11,4 @@ def random_word(length):
 url = 'https://spb.hh.ru/search/vacancy?text=Scala&only_with_salary=false&enable_snippets=true&clusters=true&area=2'
 
 for i in range(0, 20):
-    scrap(url, random_word(5), random.randint(-50, 50))
+    scrap(url, get_random_word(5), random.randint(-50, 50))
