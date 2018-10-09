bfbca43c2cb42475466d45760d6c2a5b3bb438e6
Egor Galkin <egor.galkin99@yandex.ru>
2018-10-09 14:19:40 +0300

    Encapsulate scraping from main into scrap function

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -13,34 +13,39 @@ from url_addreses import COLLECTOR_URL
 
 def main():
     argv = sys.argv
-    es = Elasticsearch()
     if len(argv) != 2:
         print('usage: <url>')
     else:
         url = argv[1]
-        response = parse_html(get_pages(url))
-        doc = {
-            'path': url
-        }
-        create_index(es, 'hh', hh_settings)
-        create_index(es, 'log', log_settings)
-        store_url_record(es, 'hh', doc, url)
-        global_hash = ''
-        for r in response:
-            global_hash += store_vacancy_record(es, 'hh', r, url)
-        hash_object = hashlib.md5(global_hash.encode())
-        changes = get_changes(es, 'log', url)
-        record = create_changes_log(url, hash_object.hexdigest(), len(response))
-        if len(changes) == 0:
+        scrap(url)
+
+
+def scrap(url, hash_suffix='', add_to_len=0):
+    es = Elasticsearch()
+    response = parse_html(get_pages(url))
+    doc = {
+        'path': url
+    }
+    create_index(es, 'hh', hh_settings)
+    create_index(es, 'log', log_settings)
+    store_url_record(es, 'hh', doc, url)
+    global_hash = ''
+    for r in response:
+        global_hash += store_vacancy_record(es, 'hh', r, url)
+    global_hash += hash_suffix
+    hash_object = hashlib.md5(global_hash.encode())
+    changes = get_changes(es, 'log', url)
+    record = create_changes_log(url, hash_object.hexdigest(), len(response) + add_to_len)
+    if len(changes) == 0:
+        store_change_record(es, 'log', record, url)
+        record['status'] = 'create'
+        requests.post(COLLECTOR_URL, data=record)
+    else:
+        cur_record_hash = changes[0]['_source']['hash']
+        if hash_object.hexdigest() != cur_record_hash:
             store_change_record(es, 'log', record, url)
-            record['status'] = 'create'
+            record['status'] = 'update'
             requests.post(COLLECTOR_URL, data=record)
-        else:
-            cur_record_hash = changes[0]['_source']['hash']
-            if hash_object.hexdigest() != cur_record_hash:
-                store_change_record(es, 'log', record, url)
-                record['status'] = 'update'
-                requests.post(COLLECTOR_URL, data=record)
 
 
 if __name__ == "__main__":
